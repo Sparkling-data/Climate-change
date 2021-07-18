4ce52e854da117dbe9a1bb632fa777b21ad0d501
Jinny <eui0326@gmail.com>
2021-07-18 20:36:35 +0900

    connection

--- app.py
+++ app.py
@@ -13,27 +13,51 @@ def start():
 
 @app.route('/main', methods=["GET"])
 def main():
-    return render_template("main.html")
+    return render_template("order.html")
 
 
-@app.route('/chart2', methods=["GET"])
+@app.route('/chart', methods=["GET"])
 def move():
-    return render_template("chart2.html")
+    return render_template("chart.html")
 
 
 @app.route('/calling', methods=["get"])
 def mchart():
 
     a = chart_dao().chart2()
-#     # 여기서 DAO 에서 판다스 만들기
-#     # mchart() == > 차트 만들기
+    b = chart_dao().chart3()
+    # 여기서 DAO 에서 판다스 만들기
+    # mchart() == > 차트 만들기
     return a  # 에서 만든 차트 보내기??  가능?? 차트 정보 보내서 html 에서 google chart?
+
+
+@app.route("/co2", methods=["get"])
+def co2():
+    return render_template("co2.html")
+
+
+@app.route("/sea_t", methods=["get"])
+def st():
+    return render_template("sea_t.html")
+
+
+@app.route("/atmo_t", methods=["get"])
+def at():
+    return render_template("atmo_t.html")
+
+
+@app.route("/height", methods=["get"])
+def height():
+    return render_template("height.html")
+
+
+@app.route("/total", methods=["get"])
+def total():
+    return render_template("total.html")
 
 
 # pandas 가서 차트 만들기
 # return render_template("chart2.html")
-
-
 if __name__ == '__main__':
     app.run(host="127.0.0.1",
             port="5000",
--- dao.py
+++ dao.py
@@ -2,6 +2,7 @@ import cx_Oracle
 import json
 import collections  # 데이터를 어떤 구조로 관리할 것이가를 의미하는 자료 구조를 지원하는 library
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class chart_dao:
@@ -50,6 +51,54 @@ class chart_dao:
         # print(data) asdfsadf sa
         # print("_________")
         return data
+
+    def chart3(sef):
+        #data = []
+        conn = cx_Oracle.connect(user="SCOTT", password="TIGER", dsn="xe")
+        cur = conn.cursor()
+        cur.execute(
+            "select H_YEAR, H_WORLD, T_TEM from sea_height, sea_tem where H_YEAR = T_YEAR")
+        conn.commit()
+        rows = cur.fetchall()
+        v = []  # v 변수에 python 구조의 dic 구조로 저장 -> data 변수로 json 포멧으로 변환
+        for row in rows:
+            # print(row)
+            d = collections.OrderedDict()
+
+            d['year'] = row[0]
+            # print(d)
+            d['sealevel'] = row[1]
+            d['co2_den'] = row[2]
+
+            v.append(d)  # 이미 존재하는 list의 마지막 요소로 저장(add)
+            print("-----------")
+        print(v)
+        data = json.dumps(v)  # json 포멧으로 자동 변환
+        # print(data)
+        df = pd.DataFrame(v)
+        print(df)
+        print(type(df))
+        df.columns = ['Year', 'Sealevel', 'Seatemgap']
+        print("---")
+        print(df.columns)
+        df.set_index('Year', inplace=True)
+        x = df.index
+        y = df["Seatemgap"]
+        a = plt.plot(x, y, color="y")
+        plt.title("Seatemgap")
+        plt.savefig("./static/image/barchart")
+        print("성공")
+        x = df.index
+        y1 = df.Sealevel
+        y2 = df.Seatemgap
+        plt.title("Sealevel and Sea Tem gap")
+        plt.plot(x, y1, label="Sealevel")
+        plt.plot(x, y2, label="Seatemgap")
+        plt.legend(loc=0)
+        plt.savefig("./static/image/barchart1")
+
+        cur.close()
+        conn.close()
 
 
 if __name__ == "__main__":
